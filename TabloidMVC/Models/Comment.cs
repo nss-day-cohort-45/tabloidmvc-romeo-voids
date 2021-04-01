a2735ebcda3f1262364fe4e2516db7ac7fba83bd
Iman Nadeem <iman.nadeem.825@gmail.com>
2021-04-01 13:31:41 -0500

    more comments

--- TabloidMVC/Models/Comment.cs
+++ TabloidMVC/Models/Comment.cs
@@ -1,48 +1,48 @@
-﻿//using System;
-//using System.ComponentModel;
-//using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
-//namespace TabloidMVC.Models
-//{
-//    public class Comment
-//    {
-//        public int Id { get; set; }
-
-
-
-//        [Required]
-//        [DisplayName("Post")]
-//        public int PostId { get; set; }
-//        public Post Post { get; set; }
+namespace TabloidMVC.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
 
 
 
-
-//        [DisplayName("Author")]
-//        public int UserProfileId { get; set; }
-//        public UserProfile UserProfile { get; set; }
-
-
-
-//        [Required]
-//        public string Subject { get; set; }
+        [Required]
+        [DisplayName("Post")]
+        public int PostId { get; set; }
+        public Post Post { get; set; }
 
 
 
 
-//        [Required]
-//        public string Content { get; set; }
+        [DisplayName("Author")]
+        public int UserProfileId { get; set; }
+        public UserProfile UserProfile { get; set; }
 
 
 
-//        public DateTime CreateDateTime { get; set; }
-
-//        [DisplayName("Published")]
-//        [DataType(DataType.Date)]
-//        public DateTime? PublishDateTime { get; set; }
+        [Required]
+        public string Subject { get; set; }
 
 
 
-    
-//    }
-//}
+
+        [Required]
+        public string Content { get; set; }
+
+
+
+        public DateTime CreateDateTime { get; set; }
+
+        [DisplayName("Published")]
+        [DataType(DataType.Date)]
+        public DateTime? PublishDateTime { get; set; }
+
+
+
+
+    }
+}
--- TabloidMVC/Repositories/ICommentRepository.cs
+++ TabloidMVC/Repositories/ICommentRepository.cs
@@ -1,13 +1,13 @@
-﻿//using System.Collections.Generic;
-//using TabloidMVC.Models;
+﻿using System.Collections.Generic;
+using TabloidMVC.Models;
 
-//namespace TabloidMVC.Repositories
-//{
-//    public interface ICommentRepository
-//    {
-//        void Add(Comment comment);
-//        List<Comment> GetAllPublishedComments();
-//        Post GetPublishedCommentById(int id);
-//        Post GetUserCommentById(int id, int userProfileId);
-//    }
-//}
+namespace TabloidMVC.Repositories
+{
+    public interface ICommentRepository
+    {
+        void Add(Comment comment);
+        List<Comment> GetAllPublishedComments();
+        Post GetPublishedCommentById(int id);
+        Post GetUserCommentById(int id, int userProfileId);
+    }
+}
