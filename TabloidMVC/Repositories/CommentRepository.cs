9d2517b0ef96461945ad1246abc12bfda1276c1e
Iman Nadeem <iman.nadeem.825@gmail.com>
2021-04-01 14:07:15 -0500

    comment sql progress

--- TabloidMVC/Repositories/CommentRepository.cs
+++ TabloidMVC/Repositories/CommentRepository.cs
@@ -20,17 +20,16 @@ namespace TabloidMVC.Repositories
                 using (var cmd = conn.CreateCommand())
                 {
                     cmd.CommandText = @"
-                       SELECT p.Id, p.Title, p.Content, 
-                              p.ImageLocation AS HeaderImage,
-                              p.CreateDateTime, p.PublishDateTime, p.IsApproved,
-                              p.CategoryId, p.UserProfileId,
-                              c.[Name] AS CategoryName,
+                       SELECT c.Id, c.PostId, c.UserProfileId, 
+                              c.Subject, c.Content,
+                              c.CreateDateTime, 
+                              p.Title AS PostTitle,
                               u.FirstName, u.LastName, u.DisplayName, 
                               u.Email, u.CreateDateTime, u.ImageLocation AS AvatarImage,
                               u.UserTypeId, 
                               ut.[Name] AS UserTypeName
-                         FROM Post p
-                              LEFT JOIN Category c ON p.CategoryId = c.id
+                         FROM Comment c
+                              LEFT JOIN Post p ON c.PostId = p.id
                               LEFT JOIN UserProfile u ON p.UserProfileId = u.id
                               LEFT JOIN UserType ut ON u.UserTypeId = ut.id
                         WHERE IsApproved = 1 AND PublishDateTime < SYSDATETIME()";
