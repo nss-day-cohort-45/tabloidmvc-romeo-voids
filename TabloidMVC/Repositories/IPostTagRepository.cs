fef9d129e36bdcee42950bffc6dcbc848e1df6b5
Katherine Hinrichs <ko.hinrichs@gmail.com>
2021-04-02 12:58:09 -0600

    Adding tags to Post

---
+++ TabloidMVC/Repositories/IPostTagRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TabloidMVC.Repositories
+{
+    public interface IPostTagRepository
+    {
+        void AddPostTag()
+    }
+}
