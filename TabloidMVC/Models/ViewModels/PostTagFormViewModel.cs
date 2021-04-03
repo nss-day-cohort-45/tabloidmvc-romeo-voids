74983369044fcd75044d5b6f203b11a5a3c98726
Katherine Hinrichs <ko.hinrichs@gmail.com>
2021-04-02 18:01:35 -0600

    Adding tags to posts... still

---
+++ TabloidMVC/Models/ViewModels/PostTagFormViewModel.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TabloidMVC.Models.ViewModels
+{
+    public class PostTagFormViewModel
+    {
+        public PostTag PostTag { get; set; }
+        public Post Post { get; set; }
+        public List<Tag> Tags { get; set; }
+    }
+}
--- TabloidMVC/Repositories/ITagRepository.cs
+++ TabloidMVC/Repositories/ITagRepository.cs
@@ -5,10 +5,10 @@ namespace TabloidMVC.Repositories
 {
     public interface ITagRepository
     {
+        List<Tag> GetAllTags();
         Tag GetTagById(int id);
         void AddTag(Tag tag);
         void UpdateTag(Tag tag);
         void DeleteTag(int id);
-        List<Tag> GetAllTags();
     }
 }
