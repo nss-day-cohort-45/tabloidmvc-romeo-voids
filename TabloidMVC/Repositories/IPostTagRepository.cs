af5eb9494fb5262eabb53d627e36d85f42c20bb3
Katherine Hinrichs <ko.hinrichs@gmail.com>
2021-04-05 09:16:11 -0600

    Working on adding tags to posts

--- TabloidMVC/Repositories/IPostTagRepository.cs
+++ TabloidMVC/Repositories/IPostTagRepository.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TabloidMVC.Models;
 
 namespace TabloidMVC.Repositories
 {
     public interface IPostTagRepository
     {
-        void AddPostTag()
+        List<PostTag> GetAllPostTagsByPostId(int postId);
+        void AddPostTag(PostTag postTag);
+        void DeletePostTag(PostTag postTag);
     }
 }
--- TabloidMVC/Repositories/PostTagRepository.cs
+++ TabloidMVC/Repositories/PostTagRepository.cs
@@ -1,11 +1,48 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TabloidMVC.Models;
 
 namespace TabloidMVC.Repositories
 {
-    public class PostTagRepository
+    public class PostTagRepository : BaseRepository, IPostTagRepository
     {
+        public PostTagRepository(IConfiguration config) : base(config) { }
+        public List<PostTag> GetAllPostTagsByPostId(int postId)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                       SELECT Id, [Name]
+                         FROM Tag
+                        WHERE id = @id";
+
+                    cmd.Parameters.AddWithValue("@id", id);
+                    var reader = cmd.ExecuteReader();
+
+                    List<PostTag> postTags = new List<PostTag>();
+
+                    while (reader.Read())
+                    {
+                        PostTag postTag = new PostTag()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            PostId = reader.GetInt32(reader.GetOrdinal("PostId")),
+                            TagId = reader.GetInt32(reader.GetOrdinal("TagId")),
+                        };
+                        postTags.Add(postTag);
+                    }
+                    reader.Close();
+                    return postTags;
+                }
+            }
+        }
+        public void AddPostTag(PostTag postTag);
+        public void DeletePostTag(PostTag postTag);
     }
 }
