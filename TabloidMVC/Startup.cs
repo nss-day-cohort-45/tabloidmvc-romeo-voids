f0761190d34489c2df1fdf5d19f054e7e724c0ae
Iman Nadeem <iman.nadeem.825@gmail.com>
2021-04-06 11:28:23 -0500

    views almost working

--- TabloidMVC/Startup.cs
+++ TabloidMVC/Startup.cs
@@ -29,6 +29,7 @@ namespace TabloidMVC
             services.AddTransient<IUserProfileRepository, UserProfileRepository>();
             services.AddTransient<ITagRepository, TagRepository>();
             services.AddTransient<ICommentRepository, CommentRepository>();
+            services.AddTransient<IUserProfileRepository, UserProfileRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
